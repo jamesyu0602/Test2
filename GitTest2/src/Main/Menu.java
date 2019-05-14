8ebe8c02684bc5eb4af30bf14edc6027d7259603
jamesyu0602 <2208698716@qq.com>
2019-05-14 21:27:39 +0800

    fd

--- GitTest2/src/Main/Menu.java
+++ GitTest2/src/Main/Menu.java
@@ -5,5 +5,6 @@ public static void main(String[] args) {
 	System.out.println("ABCDEFGHIJKLNMPQRSTUPWXYZ");
 	System.out.println("ZZZZ");
 	System.out.println("ZZZZ");
+	System.out.println("ZZZZ");
 }
 }
